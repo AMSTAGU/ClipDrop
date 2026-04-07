ba677b202551fdb01970bc804362a0247d7baf60
amstagu <amstagu@icloud.com>
2026-04-07 15:24:12 +0200

    feat: initialize new macOS project with basic ContentView and asset configuration

---
+++ Aidrop-Clipboard/Aidrop_ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aidrop_ClipboardApp.swift
+//  Aidrop-Clipboard
+//
+//  Created by Amaury Gau on 07/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Aidrop_ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aidrop-Clipboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aidrop-Clipboard
+//
+//  Created by Amaury Gau on 07/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
